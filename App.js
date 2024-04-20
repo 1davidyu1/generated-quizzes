9d4c416c07aa8bdde1d63a95696734889e0fd591
1davidyu1 <yudavid@msu.edu>
2024-04-20 12:19:47 -0400

    begin page

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Begin from "./components/Begin"
+import Quiz from "./components/Quiz"
+
+export default function App() {
+    return (
+        <main>
+            <Begin />
+        </main>
+    )
+}
