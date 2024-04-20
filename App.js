d07adb57e3d84ffeeecc1f3f5ec896204a405519
1davidyu1 <yudavid@msu.edu>
2024-04-20 19:57:35 -0400

    fetching api url

--- App.js
+++ App.js
@@ -3,19 +3,29 @@ import Quiz from "./components/Quiz"
 
 export default function App() {
     const [beginQuiz, setBeginQuiz] = useState(false)
+    const [questions, setQuestions] = useState([])
+
+    console.log(questions)
 
     // need to change
     const question = "What is?"
     const answers = "somelarge"
     const isHeld = false
-
-    function handleBegin() {
-        setBeginQuiz(true)
-    }
+    
 
     const mainBackgroundSize = {
         backgroundSize: beginQuiz ? "20%, 20%" : "40%, 40%"
     }
+    
+    function handleBegin() {
+        setBeginQuiz(true)
+    }
+
+    useEffect(() => {
+        fetch("https://opentdb.com/api.php?amount=10")
+            .then(res => res.json())
+            .then(data => setQuestions(data.results))
+    },[])
 
     return (
         <main style={mainBackgroundSize}>
